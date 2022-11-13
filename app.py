040eaac2eb6fafd1fbbd5dfa4929773c720dfa58
Sneha Madhuri Jangam <89703548+madhurijangam@users.noreply.github.com>
2022-11-14 01:49:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,12 +3,12 @@ from flask import Flask,request,jsonify,render_template
 import pickle
 
 # create flask app
-Flask_app=Flask(__name__)
+app=Flask(__name__)
 model=pickle.load(open("RandomForest.pkl", "rb"))
-@Flask_app.route("/")
+@app.route("/")
 def Home():
     return render_template("index.html")
-@Flask_app.route("/predict",methods=["POST"])
+@app.route("/predict",methods=["POST"])
 def predict():
     float_features=[float(x) for x in request.form.values()]
     features=[np.array(float_features)]
@@ -16,6 +16,6 @@ def predict():
 
     return render_template("index.html",prediction_text="The BEST CROP FITS  is {}".format(prediction))
 if __name__ == "__main__":
-    Flask_app.run(debug=True)
+    app.run(debug=True)
 
 
